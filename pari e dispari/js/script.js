5398b1040e69680fe571d70ea6c95ddcc9bf04e1
qsrdev <matteo.albertini747@gmail.com>
2025-04-22 16:34:21 +0200

    feat: quasi finito

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -3,8 +3,9 @@ let userEven;
 let userOdd;
 
 function oddoreven() {
+  console.log(`Accesso alla funzione di Richiesta se il giocatore vuole essere pari o dispari`);
   let retry = 5;
-  const userType = parseInt(prompt("Scrivi 0 per pari e 1 per dispari"));
+  const userType = parseInt(prompt("Scrivi \n 0: per pari \n 1: per dispari"));
   if (userType === 0) {
     user = userEven;
     alert(`L'utente sarà pari`);
@@ -15,25 +16,46 @@ function oddoreven() {
     alert(`Il numero inserito non va bene riprova`);
     if (--retry > 0) oddoreven();
   }
+  return user;
 }
 
 oddoreven();
 
-const userNumber = parseInt(prompt("Dimmi un numero da 1 a 5"));
-console.log(`Il numero dell'utente è ${userNumber}`);
+function userNumber() {
+  console.log(`Accesso alla funzione di richiesta del numero del giocatore`);
+  let retry = 5;
+  const userInput = parseInt(prompt("Dimmi un numero da 1 a 5"));
+  if (userInput >= 1 && userInput <= 5) {
+    console.log(`Il numero inserito dal giocatore é ${userInput}`);
+  } else {
+    alert(`Il numero inserito non va bene riprova`);
+    if (--retry > 0) userNumber();
+  }
+  return userInput;
+}
+
+userNumber();
 
 function roll() {
   console.log(`Accesso alla funzione di roll per il computer`);
   const rollCPU = Math.floor(Math.random() * 5) + 1;
+  console.log(`Il numero del computer è ${rollCPU}`);
   return rollCPU;
 }
 
-console.log(`Il numero del computer è ${roll()}`);
+roll();
 
-function vincitor() {
-  let somma = userNumber + roll();
-
-  if (somma % 2 === 0) {
-    console.log(`Vince utente`);
+function vincitore() {
+  console.log(`Calcolo del vincitore`);
+  let risultato = userNumber() + roll();
+  if (risultato % 2 === 0 && user == userEven) {
+    alert(`Vince il giocatore`);
+  } else if (risultato % 2 != 0 && user == userOdd) {
+    alert(`Vince il giocatore`);
+  } else {
+    alert(`Vince il computer`);
   }
+  return risultato;
 }
+
+vincitore();
