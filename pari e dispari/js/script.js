126452f5d5db1a93fcd362b9fefe1888af05184a
qsrdev <matteo.albertini747@gmail.com>
2025-04-22 17:33:23 +0200

    feat: correction

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -3,60 +3,80 @@ let userEven;
 let userOdd;
 let rollCPU;
 
-function oddoreven() {
-  console.log(`Accesso alla funzione di Richiesta se il giocatore vuole essere pari o dispari`);
-  let retry = 5;
-  const userType = parseInt(prompt("Scrivi \n 0: per pari \n 1: per dispari"));
-  if (userType === 0) {
-    user = userEven;
-    alert(`L'utente sarà pari`);
-  } else if (userType === 1) {
-    user = userOdd;
-    alert(`L'utente sarà dispari`);
-  } else {
-    alert(`Il numero inserito non va bene riprova`);
-    if (--retry > 0) oddoreven();
-  }
-  return user;
+const languages = {
+  it: {
+    odd: "pari",
+    even: "dispari",
+  },
+  es: {
+    odd: "paris",
+    even: "disparis",
+  },
+  en: {
+    odd: "odd",
+    even: "even",
+  },
+  fr: {
+    odd: "pariè",
+    even: "dispariè",
+  },
+};
+
+function oddoreven(userType) {
+  return userType == 0 ? "odd" : "even";
 }
 
-oddoreven();
+// const userType = prompt("Scrivi \n 0: per pari \n qualsiasi altra cosa per dispari");
+const userType = 0;
+user = oddoreven(userType);
+let number = 7;
+let isWinner = checkWin(user, number);
 
-function userNumber() {
-  console.log(`Accesso alla funzione di richiesta del numero del giocatore`);
-  let retry = 5;
-  const userInput = parseInt(prompt("Dimmi un numero da 1 a 5"));
-  if (userInput >= 1 && userInput <= 5) {
-    console.log(`Il numero inserito dal giocatore é ${userInput}`);
-  } else {
-    alert(`Il numero inserito non va bene riprova`);
-    if (--retry > 0) userNumber();
-  }
-  return userInput;
+if (isWinner) {
+  console.log("hai scelto " + languages.fr[user] + ", è uscito " + number + " per cui hai vinto");
+} else {
+  console.log("hai scelto " + languages.fr[user] + ", è uscito " + number + " per cui hai perso");
 }
 
-userNumber();
+function checkWin(user, value) {
+  if (value % 2 === 0 && user === "odd") return true;
+  if (value % 2 === 1 && user === "even") return true;
 
-function roll() {
-  console.log(`Accesso alla funzione di roll per il computer`);
-  const rollCPU = Math.floor(Math.random() * 5) + 1;
-  console.log(`Il numero del computer è ${rollCPU}`);
-  return rollCPU;
+  return false;
 }
+// function userInput() {
+//   console.log(`Accesso alla funzione di richiesta del numero del giocatore`);
+//   const userNumber = parseInt(prompt("Dimmi un numero da 1 a 5"));
+//   if (userNumber >= 1 && userNumber <= 5) {
+//     console.log(`Il numero inserito dal giocatore é ${userNumber}`);
+//   } else {
+//     alert(`Il numero inserito non va bene riprova`);
+//   }
+//   return userNumber;
+// }
 
-roll();
+// userNumber();
 
-function vincitore() {
-  console.log(`Calcolo del vincitore`);
-  let risultato = user + rollCPU;
-  if (risultato % 2 === 0 && user == userEven) {
-    alert(`Vince il giocatore`);
-  } else if (risultato % 2 != 0 && user == userOdd) {
-    alert(`Vince il giocatore`);
-  } else {
-    alert(`Vince il computer`);
-  }
-  return risultato;
-}
+// function roll() {
+//   console.log(`Accesso alla funzione di roll per il computer`);
+//   const rollCPU = Math.floor(Math.random() * 5) + 1;
+//   console.log(`Il numero del computer è ${rollCPU}`);
+//   return rollCPU;
+// }
 
-vincitore();
+// roll();
+
+// function vincitore() {
+//   console.log(`Calcolo del vincitore`);
+//   let risultato = user + rollCPU;
+//   if (risultato % 2 === 0 && user == userEven) {
+//     alert(`Vince il giocatore`);
+//   } else if (risultato % 2 != 0 && user == userOdd) {
+//     alert(`Vince il giocatore`);
+//   } else {
+//     alert(`Vince il computer`);
+//   }
+//   return risultato;
+// }
+
+// vincitore();
